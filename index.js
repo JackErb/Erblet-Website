e2c8bc918db739cbd57c9421705251f4ff327380
JackErb <ankhenac@gmail.com>
2017-06-26 14:57:37 -0700

    Added HTML Image Map
    Able to now click individual sections of the wallet

---
+++ index.js
@@ -0,0 +1,11 @@
+function trimClick() {
+  document.getElementById("aside").innerHTML = "Trim";
+}
+
+function baseClick() {
+  document.getElementById("aside").innerHTML = "Base";
+}
+
+function insideClick() {
+  document.getElementById("aside").innerHTML = "Inside";
+}
