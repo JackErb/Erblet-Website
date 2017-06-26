06698b8e76b2b6aa77c2a902dd223b1b2a7d18e7
JackErb <ankhenac@gmail.com>
2017-06-26 16:05:14 -0700

    Customization of Wallet
    Added ability to customize the wallet into any permutation of available
    colors

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
+// Get the modal
+var modal = document.getElementById("colorSelector");
+
+// When the user clicks on the 'x' button, close the modal
+var closeButton = document.getElementsByClassName("close")[0];
+
+
+var componentToChangeColor = ""
+
+document.getElementById("blueColor").addEventListener("click", function() {
+  changeColor("blue")
+}, false);
+
+document.getElementById("redColor").addEventListener("click", function() {
+  changeColor("red")
+}, false);
+
+document.getElementById("yellowColor").addEventListener("click", function() {
+  changeColor("yellow")
+}, false);
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+      closeModal();
+  }
+}
+
+"images/base_red.png"
+function changeColor(color) {
+  var images = document.getElementsByClassName(componentToChangeColor);
+  for (var i=0; i<images.length; i++) {
+    images[i].src="images/" + componentToChangeColor + "_" + color + ".png";
+  }
+}
+
+function openModal(target) {
+  modal.style.display = "block";
+
+  closeButton.innerHTML = target.charAt(0).toUpperCase() + target.slice(1) + " Color &times;";
+
+  componentToChangeColor = target;
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
 function trimClick() {
-  document.getElementById("aside").innerHTML = "Trim";
+  openModal("trim");
 }
 
 function baseClick() {
-  document.getElementById("aside").innerHTML = "Base";
+  openModal("base");
 }
 
 function insideClick() {
-  document.getElementById("aside").innerHTML = "Inside";
+  openModal("inside");
 }
