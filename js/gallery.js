737e4f7f5fb70cfe1a5eb86ac8f7c9be843d8d4c
JackErb <ankhenac@gmail.com>
2017-09-12 22:18:03 -0700

    Adjusts image descriptions

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,22 @@
 var imageData = [
   {
     'name': 'Serena Williams',
-    'description': 'a duct tape portrait of tennis star, serena williams',
+    'description': 'a duct tape portrait of tennis star, <span>serena williams</span>',
     'src': 'images/serenawilliams.jpeg'
   },
   {
     'name': 'Wallets',
-    'description': 'erblet wallets are made nearly 100% out of duct tape with only 1.4% of the wallet being a film for the id-holder',
+    'description': '<span>erblet</span> wallets are made nearly 100% out of duct tape with only 1.4% of the wallet being a film for the id-holder',
     'src': 'images/wallets.jpg'
   },
   {
     'name': 'Aesthetic Wallet',
-    'description': 'an erblet wallet is pretty much just a highly functional piece of art. here\'s a design from artist wilson rhan.',
+    'description': 'an <span>erblet wallet</span> is pretty much just a highly functional piece of art. here\'s a design from artist wilson rhan.',
     'src': 'images/walletpic.jpeg'
   },
   {
     'name': 'Great Wave Off Kanagwa',
-    'description': 'the great wave off kanagawa, made entirely in duct tape',
+    'description': 'the <span>great wave off kanagawa</span>, made entirely in duct tape',
     'src': 'images/wave.jpeg'
   },
 ]
